624a10433ed32a484e5408a1f45ff2f1bf664ca8
Roland Shum <ballwallcallall@gmail.com>
2017-10-02 23:13:15 +0800

    Roland's take on it.
    Psuedo code, with none of the database things done and any calculation.

--- foodbot.py
+++ foodbot.py
@@ -2,7 +2,7 @@ import sys
 import time
 import telepot
 from telepot.loop import MessageLoop
-from food_func import welcome_keyboard,hcts_keyboard,vcts_keyboard,acts_keyboard,nearest_canteen,canteen_keyboard_select
+from food_func import welcome_keyboard, Halal_keyboard
 #start message
 
 
@@ -12,13 +12,36 @@ def on_chat_message(msg):
 
     
     if content_type =='text':
+		if msg['text'] == 'Halal Preference':
+			#================== This means its a Halal reply ============
+			#================== 
+			# 1. Get all canteens nearby 
+			# 2. Reorder based on 1) Halal, and 2) distance
+			# 3. Entire program should end here.
+			print("I prefer Halal lol")
+		elif msg['text'] == 'No Preference':
+			#================== No halal preference ============
+			#================== Same as above ==================
+			print("I don't care haha")
+		elif msg['text'] == 'Vegeterian':
+			#============== I prefer Vegeterian ================
+			#================== Same as above ==================
+			print("I'm a fuckin Vegeterian")
 
-        
+
+		else :
+			await bot.sendMessage(chat_id, 'Welcome to NTU Food Bot, \n What can I do for you?', reply_markup=welcome_keyboard())
+    #=============== REQUEST FOR LOCATION =========================
+	elif content_type == 'Get_Location':
+		#================= Do location stuff here. Nearest dist can use Pythagoreas ===============
+		print("Do location stuff here")
+		await.bot.sendMessage(chat_id, 'Please select your food preference', reply_markup=Food_Preference_keyboard())
                         
+#============================== IF ALL GOES CALL BACK QUERY THIS SHOULD NOT BE RUNNED =======================
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, from_id, query_data)
-
+	print ('ERROR: THIS SHOULD NOT BE RUNNING - BY ROLAND')
     bot.answerCallbackQuery(query_id, text='Got it, please wait... ')
     if query_data=='hcts':
         print('yes! halal')
