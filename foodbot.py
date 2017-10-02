ad866e0821cd32575df510eafd432eac6b570c88
Roland Shum <ballwallcallall@gmail.com>
2017-10-02 22:20:37 +0800

    Roland's changes
    Restart from scratch using SR's base code. Did not use any from archon

--- foodbot.py
+++ foodbot.py
@@ -5,11 +5,6 @@ from telepot.loop import MessageLoop
 from food_func import welcome_keyboard,hcts_keyboard,vcts_keyboard,acts_keyboard,nearest_canteen,canteen_keyboard_select
 #start message
 
-#help message
-
-#canteen list init
-canfood=['can1','2','4','9','11','13','14','16']
-
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -18,26 +13,7 @@ def on_chat_message(msg):
     
     if content_type =='text':
 
-        if msg['text']=='/start':
-            bot.sendMessage(chat_id, 'Where are you now?', reply_markup=welcome_keyboard())
-            
-        elif msg['text'].find("1") == 1:
-            n=canfood.index(msg['text'])
-            bot.sendMessage(chat_id, 'Please choose your canteen', reply_markup=canteen_keyboard_select(n))
-        elif msg['text']=='Vegetarian Canteen':
-            bot.sendMessage(chat_id, 'Please choose your canteen', reply_markup=vcts_keyboard())    
-        elif msg['text']=='Any Canteen':
-            bot.sendMessage(chat_id, 'Please choose your canteen', reply_markup=acts_keyboard())
-        else:
-            bot.sendMessage(chat_id, 'Input proper command?', reply_markup=welcome_keyboard())
-
-    if content_type =='location':
-        n=nearest_canteen(msg['location']['latitude'],msg['location']['longitude'])
-        print(n)
-        bot.sendMessage(chat_id, 'The nearest is canteen'+canfood[n]+'\nChoose your destination', reply_markup=acts_keyboard())#using inline
-
         
-#for inline
                         
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
