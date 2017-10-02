5e43f35c6b1cb198c27e116c62d345470b12ff15
tansr7 <31932046+tansr7@users.noreply.github.com>
2017-10-03 02:32:05 +0800

    Error, api not updated
    Custom keyboard asking for location works, determining location via pythagorean theorem not done.
    
    inlinekeyboards for all food preferences done, but unable to run due to api not updated wtf? tried updating in cmd still unable to run.
    
    have to find out how to fetch information from database,

--- food_func.py
+++ food_func.py
@@ -2,11 +2,14 @@
 #  _*_ coding:utf8 _*_
 
 import telepot
-import time
 from telepot.namedtuple import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup, InlineKeyboardButton
 
+#might not be needed, determine if canteens are open
+import time
 
-bot = telepot.Bot('02707033:AAFbGsQBdQKN_0GMqNs-SqRco-nAda5iPfc')
+TOKEN = '402707033:AAFbGsQBdQKN_0GMqNs-SqRco-nAda5iPfc'
+
+bot = telepot.Bot(TOKEN)
 
 
 # ============= markup keyboards for 3-level requests ===============
@@ -15,26 +18,74 @@ bot = telepot.Bot('02707033:AAFbGsQBdQKN_0GMqNs-SqRco-nAda5iPfc')
 # === primary requests: service selection ===
 # by replay keyboards
 # function reutrn: selected service
-def welcome_keyboard():
-    tmp=ReplyKeyboardMarkup(keyboard=
-    [
+def Welcome_Keyboard():
+    tmp = ReplyKeyboardMarkup(keyboard = [
     [KeyboardButton(text='Get_Location', request_location=True)],
     ],
     one_time_keyboard=True
     )
     return tmp
 
-def Food_Preference_keyboard():
-	tmp = ReplyKeyboardMarkup(keyboard=
-	[
-		[KeyboardButton(text = 'Halal Preference')],
-		[KeyboardButton(text = 'Vegeterian')]
-		[KeyboardButton(text = 'No Preference')]
-	],
-	one_time_keyboard=True
-	)
-	return tmp
+def Food_Preference_Keyboard():
+    tmp = ReplyKeyboardMarkup(keyboard = [
+    [KeyboardButton(text = 'Halal Preference')],
+    [KeyboardButton(text = 'Vegeterian')]
+    [KeyboardButton(text = 'No Preference')]
+    ],
+    one_time_keyboard=True
+    )
+    return tmp
 
 
+#any canteens without halal? will have to remove
+def Halal_Preference_Keyboard():
+    tmp = InlineKeyboardMarkup(keyboard = [
+    [dict(text = 'Canteen 1', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 2', callback_data = 'hl')],
+    [dict(text = 'Canteen 4', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 9', callback_data = 'hl')],
+    [dict(text = 'Canteen 11', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 13', callback_data = 'hl')],
+    [dict(text = 'Canteen 14', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 16', callback_data = 'hl')],
+    [dict(text = 'North Hill Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'North Spine Canteen', callback_data = 'hl')],
+    [dict(text = 'South Spine Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'NIE Canteen', callback_data = 'hl')]
+    ])
+    return tmp
 
+def No_Preference_Keyboard():
+    tmp = InlineKeyboardMarkup(keyboard = [
+    [dict(text = 'Canteen 1', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 2', callback_data = 'hl')],
+    [dict(text = 'Canteen 4', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 9', callback_data = 'hl')],
+    [dict(text = 'Canteen 11', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 13', callback_data = 'hl')],
+    [dict(text = 'Canteen 14', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 16', callback_data = 'hl')],
+    [dict(text = 'North Hill Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'North Spine Canteen', callback_data = 'hl')],
+    [dict(text = 'South Spine Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'NIE Canteen', callback_data = 'hl')]
+    ])
+    return tmp        
+
+def Vegetarian_Preference_Keyboard():
+    tmp = InlineKeyboardMarkup(keyboard = [
+    [dict(text = 'Canteen 1', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 2', callback_data = 'hl')],
+    [dict(text = 'Canteen 4', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 9', callback_data = 'hl')],
+    [dict(text = 'Canteen 11', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 13', callback_data = 'hl')],
+    [dict(text = 'Canteen 14', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'Canteen 16', callback_data = 'hl')],
+    [dict(text = 'North Hill Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'North Spine Canteen', callback_data = 'hl')],
+    [dict(text = 'South Spine Canteen', callback_data = 'hl')],
+    [InlineKeyboardButton(text = 'NIE Canteen', callback_data = 'hl')]
+    ])
+    return tmp
 
--- foodbot.py
+++ foodbot.py
@@ -1,50 +1,58 @@
-import sys
 import time
+import sys
 import telepot
 from telepot.loop import MessageLoop
-from food_func import welcome_keyboard, Halal_keyboard
+from food_func import Welcome_Keyboard, Halal_Preference_Keyboard, No_Preference_Keyboard, Vegetarian_Preference_Keyboard
+
 #start message
 
 
 def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(content_type, chat_type, chat_id)
 
-    
-    if content_type =='text':
-		if msg['text'] == 'Halal Preference':
-			#================== This means its a Halal reply ============
-			#================== 
-			# 1. Get all canteens nearby 
-			# 2. Reorder based on 1) Halal, and 2) distance
-			# 3. Entire program should end here.
-			print("I prefer Halal lol")
-		elif msg['text'] == 'No Preference':
-			#================== No halal preference ============
-			#================== Same as above ==================
-			print("I don't care haha")
-		elif msg['text'] == 'Vegeterian':
+        if content_type =='text':
+                if msg['text'] == 'Halal Preference':
+                        #================== This means its a Halal reply ============
+                        #================== 
+                        # 1. Get all canteens nearby 
+                        # 2. Reorder based on 1) Halal, and 2) distance
+                        # 3. Entire program should end here.
+                        print("I prefer Halal lol")
+                        bot.sendMessage(chat_id, 'Which canteen?', reply_markup = Halal_Preference_Keyboard())
+
+			
+                elif msg['text'] == 'No Preference':
+                        #================== No halal preference ============
+                        #================== Same as above ==================
+                        print("I don't care haha")
+                        bot.sendMessage(chat_id, 'Which canteen?', reply_markup = No_Preference_Keyboard())
+
+                elif msg['text'] == 'Vegetarian':
 			#============== I prefer Vegeterian ================
 			#================== Same as above ==================
-			print("I'm a fuckin Vegeterian")
+                        print("I'm a fuckin Vegetarian")
+                        bot.sendMessage(chat_id, 'Which canteen?', reply_markup = Vegetarian_Preference_Keyboard())
 
-
-		else :
-			await bot.sendMessage(chat_id, 'Welcome to NTU Food Bot, \n What can I do for you?', reply_markup=welcome_keyboard())
-    #=============== REQUEST FOR LOCATION =========================
-	elif content_type == 'Get_Location':
+                else :#user to give location / type food preference
+                        bot.sendMessage(chat_id, 'Welcome to NTU Food Bot, \nWhat can I do for you?', reply_markup=Welcome_Keyboard())
+        #=============== REQUEST FOR LOCATION ========================
+        elif content_type == 'Get_Location':
 		#================= Do location stuff here. Nearest dist can use Pythagoreas ===============
-		print("Do location stuff here")
-		await.bot.sendMessage(chat_id, 'Please select your food preference', reply_markup=Food_Preference_keyboard())
-                        
+                
+                print("Do location stuff here")
+                bot.sendMessage(chat_id, 'Please select your food preference', reply_markup=Food_Preference_keyboard())
+
 #============================== IF ALL GOES CALL BACK QUERY THIS SHOULD NOT BE RUNNED =======================
 def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    print('Callback Query:', query_id, from_id, query_data)
-	print ('ERROR: THIS SHOULD NOT BE RUNNING - BY ROLAND')
-    bot.answerCallbackQuery(query_id, text='Got it, please wait... ')
-    if query_data=='hcts':
-        print('yes! halal')
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        print('Callback Query:', query_id, from_id, query_data)
+
+        print ('ERROR: THIS SHOULD NOT BE RUNNING - BY ROLAND, testing inline kb m8')
+        bot.answerCallbackQuery(query_id, text='Got it, please wait a fucking moment... ')
+
+        if query_data=='hl':
+                print('yes! halal')
 
 
 TOKEN = '402707033:AAFbGsQBdQKN_0GMqNs-SqRco-nAda5iPfc'
@@ -53,9 +61,9 @@ bot = telepot.Bot(TOKEN)
 
 #let it auto receive msg
 MessageLoop(bot, {'chat': on_chat_message,
-                  'callback_query': on_callback_query}).run_as_thread()
+				  'callback_query': on_callback_query}).run_as_thread()
 print ('Listening ...')
 
 # Keep the program running.
 while 1:
-    time.sleep(10)
+	time.sleep(10)
